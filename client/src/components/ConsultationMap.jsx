17a3ee8de6e40490beb9acd2c015493692d24e55
cryo-cell <cryobabbi@gmail.com>
2025-09-18 14:31:34 -0400

    reveal marker icon

--- client/src/components/ConsultationMap.jsx
+++ client/src/components/ConsultationMap.jsx
@@ -1,3 +1,17 @@
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// override default icon
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 import { MapContainer, TileLayer, Marker, Circle } from "react-leaflet";
 import { useState } from "react";
 
